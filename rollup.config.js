f3ac24ca954ce88b4b3dc7b9cf5291c2988fccae
Jesper Hasselquist <jesper.hasselquist@telavox.se>
2020-10-24 15:09:31 +0900

    Rework deploy flow

--- rollup.config.js
+++ rollup.config.js
@@ -39,7 +39,7 @@ export default {
         sourcemap: true,
         format: "iife",
         name: "app",
-        file: "docs/build/bundle.js",
+        file: "public/build/bundle.js",
     },
     plugins: [
         svelte({
@@ -74,7 +74,7 @@ export default {
 
         // Watch the `public` directory and refresh the
         // browser on changes when not in production
-        !production && livereload("docs"),
+        !production && livereload("public"),
 
         // If we're building for production (npm run build
         // instead of npm run dev), minify
